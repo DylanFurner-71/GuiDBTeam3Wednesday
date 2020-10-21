405b5b5ec961dc81ba9b1b6d57cfcd4de37f6262
DylanFurner-71 <jdfurner@gmail.com>
2020-10-20 22:46:04 -0500

    landing is like 60% done

--- Project/frontend/src/components/landing.js
+++ Project/frontend/src/components/landing.js
@@ -1,46 +1,35 @@
 import React, {Component} from "react";
+import {Row, Col, Container} from "react-bootstrap"
 import {Link} from "react-router-dom";
-
+import {LoginCard} from "./LoginCard"
 class Landing extends Component {
     render() {
         return (
             <div style={{ height: "75vh" }} className="justify-content-center container valign-wrapper">
-                <div className="row">
+                <div clas   sName="row">
                     <div className="col center-align">
-                        <img style={{ width: "100px", height: "100px"}} src="https://via.placeholder.com/150
-
-C/O https://placeholder.com/" alt="Logo" />
-                        <h1>
+                                                <h1>
                             <b>Welcome</b> to Newber Eats
                         </h1>
                         <p className="flow-text grey-text text-darken-1">
                              Your newest undifferentiated food delivery app!
                         </p>
-                        <div>
-                            <Link
-                                to="/login"
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    padding: "12px"
-                                }}
-                                className="btn btn-large btn-flat waves-effect blue black-text"
-                            >Log In</Link>
-                            <Link
-                                to="/register"
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    padding: "12px"
-                                }}
-                                className="btn btn-large btn-flat waves-effect blue black-text"
-                            >Register</Link>
-                        </div>
-                    </div>
+                     <Container fluid>
+                        <Row>
+                        <Col>                     <LoginCard accountType={"Customer"}/>
+                    </Col>
+                        <Col> <LoginCard accountType={"Restaurant Employee"}/></Col>
+                        </Row>
+                        <Row>
+                        <Col>
+                     <LoginCard accountType={"Delivery"}/>
+                     </Col>
+                        <Col><LoginCard accountType={"Web Manager"}/></Col>
+                        </Row>
+                        </Container>
                 </div>
             </div>
+        </div>
         );
     }
 }
