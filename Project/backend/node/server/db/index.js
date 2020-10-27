795f8ca0a6c9c73d4eef839a64b714d88cc4e4f6
akwCode <akwCode@users.noreply.github.com>
2020-10-27 13:06:20 -0500

    restructure backend file layout

--- Project/backend/node/server/db/index.js
+++ Project/backend/node/server/db/index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
+const util = require('util');
 
-mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 10,
     user: 'manager',
     pasword: 'Password',
@@ -9,3 +10,15 @@ mysql.createPool({
     port: '3306'
 });
 
+//Attempting to connect to the database.
+pool.getConnection((err, connection) => {
+    if (err)
+        console.error("Error with database connection")
+    if (connection)
+        connection.release()
+    return;
+});
+
+pool.query = util.promisify(pool.query)
+
+module.exports = pool;
--- Project/backend/node/server/server.js
+++ Project/backend/node/server/server.js
@@ -1,52 +1,37 @@
 const express = require('express');
 const mysql = require('mysql');
-const apiRouter = require('./routes');
+const router = require('./routes');
+const app = express();
 
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+const logger = log({ console: true, file: false, label: config.name });
 
-//set up some configs for express.
+//express configs
 const config = {
   name: 'sample-express-app',
   port: 8000,
   host: '0.0.0.0',
 };
 
-//create the express.js object
-const app = express();
+//body parser
+app.use(express.urlencoded({ extended: false }))
 
-//create a logger object.  Using logger is preferable to simply writing to the console.
-const logger = log({ console: true, file: false, label: config.name });
-
-app.use(bodyParser.json());
 app.use(cors({
   origin: '*'
 }));
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-
-//mysql connection
-var connection = mysql.createConnection({
-  host: 'backend-db',
-  port: '3306',
-  user: 'manager',
-  password: 'Password',
-  database: 'db'
-});
-
-//Attempting to connect to the database.
-connection.connect(function (err) {
-  if (err)
-    logger.error("Cannot connect to DB!");
-  logger.info("Connected to the DB!");
-});
-
+//routing
 app.use('/', apiRouter)
 
-//connecting the express object to listen on a particular port as defined in the config object.
+
+//setup server
 app.listen(config.port, config.host, (e) => {
   if (e) {
     throw new Error('Internal Server Error');
   }
   logger.info(`${config.name} running on ${config.host}:${config.port}`);
 });
+
+module.exports = app;
