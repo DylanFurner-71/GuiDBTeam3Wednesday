5b61b4b23fabcbade6e221b4f1003e3de0ddfc11
akwCode <akwCode@users.noreply.github.com>
2020-10-26 23:09:36 -0500

    file restructure

---
+++ Project/backend/node/server/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+
+//set up some configs for express.
+const config = {
+  name: 'sample-express-app',
+  port: 8000,
+  host: '0.0.0.0',
+};
+
+//create the express.js object
+const app = express();
+
+//create a logger object.  Using logger is preferable to simply writing to the console.
+const logger = log({ console: true, file: false, label: config.name });
+
+app.use(bodyParser.json());
+app.use(cors({
+  origin: '*'
+}));
+app.use(ExpressAPILogMiddleware(logger, { request: true }));
+
+
+//mysql connection
+var connection = mysql.createConnection({
+  host: 'backend-db',
+  port: '3306',
+  user: 'manager',
+  password: 'Password',
+  database: 'db'
+});
+
+//Attempting to connect to the database.
+connection.connect(function (err) {
+  if (err)
+    logger.error("Cannot connect to DB!");
+  logger.info("Connected to the DB!");
+});
+
+
+//GET /
+app.get('/', (req, res) => {
+  res.status(200).send('Go to 0.0.0.0:3000.');
+});
+
+app.get('/login/:id', (req, res) => {
+  if (req.params.id == 'root@email.com')
+    res.send('pass')
+});
+
+
+
+
+//connecting the express object to listen on a particular port as defined in the config object.
+app.listen(config.port, config.host, (e) => {
+  if (e) {
+    throw new Error('Internal Server Error');
+  }
+  logger.info(`${config.name} running on ${config.host}:${config.port}`);
+});
