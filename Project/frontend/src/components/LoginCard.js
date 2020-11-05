4a4b2dad47b0e2f07a23ab365b5966c9f65fb0d8
jdtynes <jadon.tynes25@gmail.com>
2020-11-04 19:09:51 -0600

    Merge Conflict Solve

--- Project/frontend/src/components/LoginCard.js
+++ Project/frontend/src/components/LoginCard.js
@@ -6,35 +6,6 @@ import login from "./login";
 export const LoginCard = (props) => {
     return (
         <>
-<<<<<<< HEAD
-            <div className="p-3 mb-2 bg-primary text-white">
-                <div className="d-flex flex-column">
-                    <div className="p-2">{props.accountType}</div>
-                    <div className="p-2">
-                        <div className="d-flex flex-row text-white">
-                            <div className="p-2 mb-2  pb-10% pt-10%">
-                                <Link
-                                    to={`/login`}
-                                    style={{
-                                        width: "140px",
-                                        borderRadius: "3px",
-                                        fontWeight: "bold"
-                                    }}
-                                    className="btn btn-large btn-flat waves-effect white black-text"
-                                >Log In</Link>
-                            </div>
-                            <div className="p-2 mb-2 ">
-                                <Link
-                                    to={`/${props.accountType}/register`}
-                                    style={{
-                                        width: "140px",
-                                        borderRadius: "3px",
-                                        fontWeight: "bold"
-                                    }}
-                                    className="btn btn-large btn-flat waves-effect white black-text"
-                                >Register</Link>
-                            </div>
-=======
         <div className="row mr-3">
             <div className="col-1"></div>
             <div className="col-10 p-1 mb-2 bg-green text-white">
@@ -59,7 +30,6 @@ export const LoginCard = (props) => {
                                 }}
                                 className="btn-large btn-flat waves-effect white black-text"
                             >Register</Link>
->>>>>>> 14147131c0bba429824b55f1139469d00d0c2a39
                         </div>
                     </div>
             </div>
