fe3b5bef674392ec66955f039eda9cd59cb0fe74
akwCode <akwCode@users.noreply.github.com>
2020-12-01 15:57:31 -0600

    fixed account_type type mismatch

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -83,13 +83,14 @@ app.post('/register/:account_type', function (req, res) {
   let password = red.body.password;
   let type = req.params.account_type;
   let org = req.params.org;
+  let admin = req.params.adminCode
   
   connection.query('SELECT * FROM Accounts JOIN Contact ON Accounts.account_id = Contact.account_id WHERE email = ? AND password = ?', [email, password],
       function(err, result) {
         if (result.length == 0) {
           switch(type) {
             case 0: 
-              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username) VALUES (?, ?, ?, ?, ?)', [first, last, password, account_type, username],
+              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username) VALUES (?, ?, ?, ?, ?)', [first, last, password, "user", username],
               function (err, result) {
                 if (err)
                   throw err;
@@ -98,7 +99,7 @@ app.post('/register/:account_type', function (req, res) {
               });
               break;
             case 1:
-              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username) VALUES (?, ?, ?, ?, ?)', [first, last, password, account_type, username],
+              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username) VALUES (?, ?, ?, ?, ?)', [first, last, password, "driver", username],
               function (err, result) {
                 if (err)
                   throw err;
@@ -107,7 +108,7 @@ app.post('/register/:account_type', function (req, res) {
               });
               break;
             case 2:
-              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username, org) VALUES (?, ?, ?, ?, ?)', [first, last, password, account_type, username, org],
+              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username, org) VALUES (?, ?, ?, ?, ?)', [first, last, password, "employee", username, org],
               function (err, result) {
                 if (err)
                   throw err;
@@ -116,7 +117,7 @@ app.post('/register/:account_type', function (req, res) {
               });
               break;
             case 3:
-              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username, org) VALUES (?, ?, ?, ?, ?)', [first, last, password, account_type, username, "admin"],
+              connection.query('INSERT INTO Accounts (first_name, last_name, password, account_type, username, org) VALUES (?, ?, ?, ?, ?)', [first, last, password, "admin", username, adminCode],
               function (err, result) {
                 if (err)
                   throw err;
